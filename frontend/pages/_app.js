bf0833d084dbfac1da0bb862314d3cf8011326d8
Tom de Bruin <tomdebruin@gmail.com>
2021-05-09 16:08:33 +0100

    8

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from 'prop-types';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
