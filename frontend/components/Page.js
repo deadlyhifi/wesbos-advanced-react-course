89a4e985f928fdc13c2aa5cf63ee80e169fcdf22
Tom de Bruin <tomdebruin@gmail.com>
2021-05-09 13:37:12 +0100

    4

---
+++ frontend/components/Page.js
@@ -0,0 +1,7 @@
+import PropTypes from 'prop-types';
+
+export default function Page({ children }) {
+  return <div>{children}</div>;
+}
+
+Page.propTypes = { children: PropTypes.element.isRequired };
